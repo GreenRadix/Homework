85409c8d3330d282e24c48cce20d9829bf21c302
GreenRadix <83420474+GreenRadix@users.noreply.github.com>
2021-08-14 13:47:53 +0300

    Add files via upload
    Вирішення проблеми з наслідковуванням

--- caller_main.py
+++ caller_main.py
@@ -1,34 +1,54 @@
+# Імпортуємо наш графічний інтерфейс з файлу
+from caller_UI import Ui_SIMCaller
 import os
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QTextBrowser
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 
-from caller_UI import Ui_SIMCaller
 
-# class SIMCaller(Ui_SIMCaller):
-#     def __init__(self):
-#         super().__init__()
-#         self.initUI()
-#         print('Simcaller loaded step2')
-#
-#     def initUI(self):
-#
-#         self.tb = QTextBrowser()
-#         self.tb.setAcceptRichText(True)
-#         self.tb.setOpenExternalLinks(True)
-#
-#         self.FindPortButtMod_1.clicked.connect(print('pohj.mfjfgjf'))
-#
-#     def bp(self):
-#         print('Ghbdsn ')
-#         # textBrowserMod_1
+
+class SIMCaller(QtWidgets.QMainWindow):
+
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+
+        self.ui = Ui_SIMCaller()
+        self.ui.setupUi(self)
+        # Описуємо реакції на натискання кнопок
+        self.ui.FindPortButtMod_1.clicked.connect(self.FindPort)
+        self.ui.ConnButtMod_1.clicked.connect(self.Connect)
+        self.ui.DisconnButtMod_1.clicked.connect(self.Disconnect)
+        self.ui.FindPortButtMod_2.clicked.connect(self.FindPort)
+        self.ui.ConnButtMod_2.clicked.connect(self.Connect)
+        self.ui.DisconnButtMod_2.clicked.connect(self.Disconnect)
+
+    def FindPort(self):
+        print("Find ports button click")
+        text = "Find ports button click"
+        self.ui.textBrowserMod_1.append(text)
+
+    def Connect(self):
+        print("ConnButtMod_1.clicked")
+        text = "ConnButtMod_1.clicked"
+        self.ui.textBrowserMod_1.append(text)
+
+    def Disconnect(self):
+        print("DisconnButtMod_1.clicked")
+        text = "DisconnButtMod_1.clicked"
+        self.ui.textBrowserMod_1.append(text)
+
+
+
+
+    # def bp(self):
+    #     print('Ghbdsn ')
+    #     # textBrowserMod_1
 
 
 class Modem:
     def __init__(self):
         pass
-
 
 class ATcommands:
     pass
@@ -39,23 +59,22 @@ class Connector:
         pass
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    SIMCaller = QtWidgets.QMainWindow()
-    ui = Ui_SIMCaller()
-    ui.setupUi(SIMCaller)
-    SIMCaller.show()
-
-#-----------------------------------------------------------------------------
-
-
-    # ui.FindPortButtMod_1.clicked.connect(bp)
-    # ui.FindPortButtMod_2.clicked.connect(bp)
-
-
-
-
-
-#-----------------------------------------------------------------------------
-
+    myapp = SIMCaller()
+    myapp.show()
     sys.exit(app.exec_())
+#-----------------------------------------------------------------------------
+
+    # class SIMCaller(Ui_SIMCaller):
+# -----------
+# if __name__ == "__main__":
+    #     app = QtWidgets.QApplication(sys.argv)
+    #     SIMCaller = QtWidgets.QMainWindow()
+    #     ui = Ui_SIMCaller()
+    #     ui.setupUi(SIMCaller)
+    #     SIMCaller.show()
+    #     sys.exit(app.exec_())
+#-----------------------------------------------------------------------------
+
+
